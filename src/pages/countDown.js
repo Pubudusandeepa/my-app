8125d6509b0f4e490edf9dadc5d7320cafd56a3e
Pubudusandeepa <ppsandeepa12@gmail.com>
2023-04-25 13:08:05 +0530

    comit

---
+++ src/pages/countDown.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const CountDown = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementValue = () => {
+    setCount(count + 1);
+  };
+
+  const decrementValue = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <div>
+        <p>count: {count}</p>
+        <button
+          style={{ background: "light-blue", padding: "5px" }}
+          onClick={() => incrementValue()}
+        >
+          Increment
+        </button>
+        <button style={{ background: "red" }} onClick={() => decrementValue()}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CountDown;
